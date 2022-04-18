ae15d2f9c2773feb376f22f76dbb9cf41cf4cd49
Marian Paul <marian.paul2@gmail.com>
2022-04-17 20:17:58 -0400

    Initial Commit

---
+++ SwissborgTickers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwissborgTickers
+//
+//  Created by Marian Paul on 2022-04-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwissborgTickers/SwissborgTickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwissborgTickersApp.swift
+//  SwissborgTickers
+//
+//  Created by Marian Paul on 2022-04-17.
+//
+
+import SwiftUI
+
+@main
+struct SwissborgTickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
